e44b2ce8e23405a7118fa51507b71468e28e976d
Kevin Urgilés <kevinurgiles1996@gmail.com>
2020-11-20 15:28:37 -0500

    Starter files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { Login } from './components/Login';
+import { SignUp } from './components/SignUp';
+import { Home } from './components/Home';
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router >
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/login' component={Login} />
+          <Route path='/signup' component={SignUp} />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
