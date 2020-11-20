93dd5386a84611e1cf78da322ef5877b40f2e6c8
Kevin Urgilés <kevinurgiles1996@gmail.com>
2020-11-20 16:01:11 -0500

    Context added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import './App.css';
+
 import { Login } from './components/Login';
 import { SignUp } from './components/SignUp';
 import { Home } from './components/Home';
+
+import { AuthProvider } from './context/AuthContext';
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <Router >
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route exact path='/login' component={Login} />
-          <Route exact path='/signup' component={SignUp} />
-        </Switch>
+      <Router>
+        <AuthProvider>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/login' component={Login} />
+            <Route exact path='/signup' component={SignUp} />
+          </Switch>
+        </AuthProvider>
       </Router>
     </>
   );
